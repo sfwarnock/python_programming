d3ff24b5a4d55e6c7d0ab05bee79dbf7f07c4e4c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-03-19 19:47:34 -0600

    Add files via upload

---
+++ chapter 1/chaos.py
@@ -0,0 +1,8 @@
+# File: chaos.py
+# A simple program illustrating chaotic behavior.
+
+print("THis progam illustrates a chotic function")
+x = eval(input("Enter a number between 0 and 1: "))
+for i in range(10):
+    x = 3.9 * x * (1-x)
+    print (x)
