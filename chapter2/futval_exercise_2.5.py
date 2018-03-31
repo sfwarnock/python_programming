c01a1875349da8f920c107325e1497e4337d2b39
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-03-31 10:29:34 -0600

    Add files via upload

---
+++ chapter2/futval_exercise_2.5.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 19:41:07 2018
+
+@author: Scott Warnock
+"""
+
+# futval_2.5.py
+#   A program to compute the value of an investment carried as many years into the future as the user enters.
+
+def main():
+    print("This program calculate the future value of a investment over a user selected duration.")
+    
+    p = eval(input("Enter the initial principal: "))
+    apr = eval(input("Enter the annual interest rate: "))
+    x = eval(input("Enter the number of years of our investment: "))
+    
+    for i in range(x):
+        p = p * (1 + apr)
+        
+    print("The value in", x, "years is:", p)
+main()
