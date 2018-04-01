ded575ec79bee40621fb8fd7ce6ef7f32f208bbd
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-01 17:45:11 -0600

    Add files via upload

--- chapter2/futval_exercise_2.6.py
+++ chapter2/futval_exercise_2.6.py
@@ -17,7 +17,11 @@ def main():
     a = eval(input("Enter the yearly contrubution after year one: "))
     
     for i in range(x):
-        p = (p * (1 + apr)) + a
+        p = p * (1 + apr)
+        a = a * (1 + apr)
         
-    print("The value in",x, "years is:", p)
+    #print("The value in",x, "years is:", p)
+    
+    print(p)
+    print(a)
 main()
