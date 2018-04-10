9494f68a35edbcc7a0685bb53f5c39e461f670e9
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-10 06:42:15 -0600

    Add files via upload

---
+++ chapter 3/exercise_3.1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 06:14:39 2018
+
+@author: Scott Warnock
+"""
+
+# 3.1
+#   Write a program to calculate the volume and surface area of a sphere from its radius, given as input.
+#   Here are some some formulas that might be useful.
+#   V = 4/3pi^3
+#   A = 4pir^2
+
+import math
+
+def main():
+    r = eval(input("Enter the radius, in inches, of the sphere: "))
+    p = math.pi
+    v = (4/3) * p * (r **3)
+    a = 4 * p * (r ** 2)    
+    print("The volume of the given sphere is: ",round(v, 3),"inches cubed.  And the are of the given sphere is ", round(a, 3), "inches squared.")
+main()
