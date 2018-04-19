de2563e0b4ca3c1e42acaa4c950a79febb636516
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-19 15:43:54 -0600

    Add files via upload

---
+++ chapter 3/exercise_3.13.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19
+
+@author: 175272
+"""
+
+# Exercise 3.13
+#
+#   Write a program to sum a series of numbers entered by the user.  The program should
+#   first prompt the user for how many numbers are to be summed.  It should then input each
+#   of the numbers and print a total sum.
+
+print("This program sums numbers entered by a user")
+print()
+
+def main():
+    tn = eval(input("How many numbers do you want to sum? "))
+    sum = 0
+    for n in range(tn):
+        n = eval(input("Enter a number: "))
+        sum = sum + n
+    print()    
+    print ("The total sum of the numbers you entered is", sum)
+main()
