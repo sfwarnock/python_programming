5ec75cd15944d3a031e0311921435ba07bd53762
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-19 15:48:32 -0600

    Add files via upload

---
+++ chapter 3/exercise_3.14.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19
+
+@author: 175272
+"""
+
+# Exercise 3.14
+#
+#   Write a program that finds the average of a series of numbers entered by the user.
+#   First prompt the user for how many numbers are to be entered..
+
+print("This program averages numbers entered by the user.")
+print()
+
+def main():
+    tn = eval(input("How many numbers do you want to average? "))
+    sum = 0
+    for n in range(tn):
+        n = eval(input("Enter a number: "))
+        sum = sum + n
+        mean = sum / tn
+    print()    
+    print ("The mean of the numbers you entered is", mean)
+main()
