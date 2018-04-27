e0452cbea527bb74acc2e3a83626fa5637a86960
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-27 12:55:24 -0600

    Add files via upload

---
+++ chapter 5/dateconvert2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 27 12:36:55 2018
+
+@author: Scott Warnock
+"""
+
+# dateconvert2.py
+#
+#   Converts day month and year numbers into two date formats
+
+def main():
+    # get the day month and year
+    day, month, year = eval(input("Enter day, month, and year numbers: "))
+    
+    date1 = str(month) + "/" + str(day) + "/" + str(year)
+    
+    months = ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"]
+    
+    monthStr = months[month-1]
+    
+    date2 = monthStr + " " + str(day) + ", " + str(year)
+    
+    print("The date is", date1, "or", date2+".")
+    
+main()
