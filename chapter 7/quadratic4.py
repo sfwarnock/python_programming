12aed8887b9137a4deaa305b6221b183e5b2fbdf
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-05-10 06:17:41 -0600

    Add files via upload

---
+++ chapter 7/quadratic4.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 2018
+
+@author: Scott Warnock
+"""
+
+# quadratic4.py
+
+import math
+
+def main():
+    print("This program finds the real solutions to a quadratic\n")
+    
+    a, b, c = eval(input("Please enter the coefficients (a,b,c): "))
+    
+    discrim = (b * b) - 4 - (a * c)
+    if discrim < 0:
+        print("\nThe equation has no real roots!")
+    elif discrim == 0:
+        root = -b / (2 * a)
+        print("\nThere is a double root at", root)
+    else:
+        discRoot = math.sqrt(discrim)
+        root1 = (-b + discRoot) / (2 * a)
+        root2 = (-b - discRoot) / (2 * a)
+        print("\nThe solutions are:", round(root1,3), round(root2,3))
+
+main()
