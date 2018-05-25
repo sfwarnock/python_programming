5a51099f25f8a5f33a54defd7454e8d60df3826c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-05-25 12:18:20 -0600

    Add files via upload

---
+++ chapter 8/average3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 12:07:48 2018
+
+@author: Scott Warnock
+"""
+
+# average3.py
+
+def main():
+    sum = 0.0
+    count = 0
+    x = eval(input("Enter a number (negative to quit): "))
+    
+    while x >= 0:
+        sum = sum + x
+        count = count + 1
+        x = eval(input("Enter a number (negative to quit): "))
+    print("\nThe average of the numbers is", sum / count)
+    
+main()
