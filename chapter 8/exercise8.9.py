d813a50792b7f083486b731d3466dba416e260e9
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-06-18 07:51:34 -0600

    Add files via upload

---
+++ chapter 8/exercise8.9.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 2018
+
+@author: Scott Warnock
+"""
+
+# exercise8.9.py
+#
+#   Write a program that computes the fuel efficiency of a multi-leg journey.
+#   The program will first prompt the for the starting odometer reading and then
+#   get information about a series of legs.  For each leg, the user enters the odometer
+#   reading and the amount of gas used (separated by a space).  The user signals the end
+#   of the trip with a blank line.  The program should print out the miles per gallon
+#   achived on each leg and the total MPG for the trip.
+
+def main():
+    
