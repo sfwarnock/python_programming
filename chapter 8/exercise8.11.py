6dd91e17df1930fa57e55382e90d49e21fa542b9
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-07-19 15:31:13 -0600

    Add files via upload

---
+++ chapter 8/exercise8.11.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 2018
+
+@author: Scott Warnock
+"""
+
+# exercise8.11.py
+
+# Heating and cooling degree-days are measures used by utility companies to estimate
+#   energy requirements. If the avergae temperature for a day is below 60, then the 
+#   number of degrees below 60 is added to the heating degree-days. If the temperature
+#   is above 80, the amount over 80 is added to the cooling days. Write a program that 
+#   accepts a sequence of the average daily temps and computes the running total of cooling
+#   heating degree-days. The program should print these two totals after all the data has been processed.
+
+def main():
+    
+    
